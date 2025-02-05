8f1719d77252141799518f4389b8f70e6ce4d8c3
Coos Baakman <coos.baakman@radboudumc.nl>
2025-02-05 11:49:58 +0000

    do the 1-channel to 3-channel conversion

--- mnist_vae/tools/image.py
+++ mnist_vae/tools/image.py
@@ -5,6 +5,9 @@ from matplotlib import pyplot
 
 def save_image(image: torch.Tensor, path: str):
 
+    # convert 1-channel to 3-channel
+    image = torch.concat((image, image, image), dim=0)
+
     pil = torchvision.transforms.ToPILImage()(image)
 
     pyplot.imshow(pil)
